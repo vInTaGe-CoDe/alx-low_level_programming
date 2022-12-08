94cf58f712299e5f552bdbb4be89dfdb87b43582
vInTaGe-CoDe <tophictango@outlook.com>
2022-12-08 14:50:31 -0800

    printf betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,10 @@
  *
  *  Return: 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return(0);
 }
