4b908f58aad732b8ba7d46882e78da642832a139
vInTaGe-CoDe <tophictango@outlook.com>
2022-12-11 07:31:56 -0800

    Single digit numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
