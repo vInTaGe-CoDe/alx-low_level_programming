23de325ff0033a727300b251f7363a107ae5584c
vInTaGe-CoDe <tophictango@outlook.com>
2022-12-13 21:08:19 -0800

    Print every minute of Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*jack_bauer - prints every minute of the day
+*
+*Return: 0
+*/
+
+void jack_bauer(void)
+{
+	int min, hour;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+		}
+	}
+}
