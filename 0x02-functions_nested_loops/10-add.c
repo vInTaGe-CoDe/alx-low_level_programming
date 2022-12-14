e9cfe8ad8d6ee7350b9a75287196921fd124b2d2
vInTaGe-CoDe <tophictango@outlook.com>
2022-12-13 21:12:47 -0800

    Adds two integers and retuwns result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @n1: The first integer
+ * @n2: The second integer
+ *
+ * Return: The result
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
