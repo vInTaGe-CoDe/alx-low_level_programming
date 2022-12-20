4277c4b6ac1969361f042b71725ebfd86733bb54
vInTaGe-CoDe <tophictango@outlook.com>
2022-12-20 03:35:01 -0800

    Generate random passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+ *
+ *  * main - program that generates random valid
+ *
+ *   * passwords for the program 101-crackme
+ *
+ *    *
+ *
+ *     * Return: 0
+ *
+ *      */
+
+int main(void)
+
+{
+
+	int pass[100];
+
+	int i, sum = 0, n;
+
+
+
+	srand(time(NULL));
+
+
+
+	for (i = 0; i < 100; i++)
+
+	{
+
+		pass[i] = rand() % 78;
+
+		sum += (pass[i] + '0');
+
+		putchar(pass[i] + '0');
+
+		if ((2772 - sum) - '0' < 78)
+
+		{
+
+			n = 2772 - sum - '0';
+
+			sum += n;
+
+			putchar(n + '0');
+
+			break;
+
+		}
+
+	}
+
+
+
+	return (0);
+
+}
