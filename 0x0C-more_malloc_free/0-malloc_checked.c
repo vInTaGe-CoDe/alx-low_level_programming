d8f40846dbb2f1f4c47e7cd954d1089087592bbc
vInTaGe-CoDe <tophictango@outlook.com>
2023-01-12 07:17:49 -0800

    Allocate memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,19 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
+ * malloc_checked - cause normal process termination with a status value of 98
+ *
+ * @b: allocated memory
  *
  * Return: a pointer to the allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *i;
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
+	i = malloc(b);
+	if (i == NULL)
 		exit(98);
+	return (i);
 
-	return (ptr);
 }
