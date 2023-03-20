4fed7d828628381d79838a2f8e8282b86849a18f
vInTaGe-CoDe <tophictango@outlook.com>
2023-03-20 14:26:53 -0700

    dyanmic lib

--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _strlen - a function that returns the length of a string.
- * @str: The string to get the length of.
- * Return: The length of the string
+ * _strlen - Function that returns the length of a string
+ *
+ * @s: this is the input string
+ *
+ * Return: Lenngth of the string
  */
-
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int index;
 
-	while (*str++)
-		length++;
-
-	return (length);
+	for (index = 0; s[index] != '\0' ; index++)
+		;
+	return (index);
 }
