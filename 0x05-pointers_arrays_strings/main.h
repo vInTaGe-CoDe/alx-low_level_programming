f7ab57ed678e0e5a3b2fbc5420aa113cda524915
vInTaGe-CoDe <tophictango@outlook.com>
2023-03-20 14:33:56 -0700

    main h file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,18 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * File: main.h
- * Author: Raymond Omoraka
- * Desc: Header file containing the prototypes of all functions and the prototype of the function _putchar
- */
-
-#include <stdlib.h>
+#ifndef MAIN_H_
+#define MAIN_H_
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -21,5 +13,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
 #endif
